d4cf33306d8339ba51ce9f3c4f1a9e4215e6e114
Manas <gupta.manas6268@gmail.com>
2023-10-01 13:44:22 +0530

    Updated

---
+++ c++_language/Codeforces/800_to_1200/0010_1864_A_Easy_problem.cpp
@@ -0,0 +1,34 @@
+// Q.10: https://codeforces.com/problemset/problem/1864/A
+
+
+
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long 
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+signed main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    int tc; cin >> tc;
+
+    while (tc--){
+        int x, y, n; cin >> x >> y >> n;
+        int a;
+
+        if((y-x) >= ((n * (n-1))/2)){
+            a = ((y-x)/ ((n * (n-1))/2));
+
+            
+        }
+    }
+}
