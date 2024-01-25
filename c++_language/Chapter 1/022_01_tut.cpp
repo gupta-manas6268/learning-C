0cfd09467031238a2be6c0e3347eb11dd770f1fe
Manas <gupta.manas6268@gmail.com>
2024-01-25 23:11:59 +0530

    Updated

---
+++ c++_language/Chapter 1/022_01_tut.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // OOPs - Classes and objects
+
+    // C++ --> initially called --> C with classes by 'Strous Troup'
+    // Class --> Extension of structures (in C)
+
+    // Structures --> had limitations
+    // Structures --> members are public
+    // Structures --> No methods
+
+    // Classes --> Structures + more
+    // Classes --> can have methods and properties
+    // Classes --> can make few members as private & few as public
+
+    // Structures in C++ are already 'typedefed' as we don't need to use 'typedef' in C++.
+
+    // You can declare Objects along with the class declaration like this:
+        class Employee{
+            // Class definition
+        } manas, harry, rohan;
+
+    // manas.salary = 8; // It makes no sense if salary is private.
+
+    return 0;
+}
