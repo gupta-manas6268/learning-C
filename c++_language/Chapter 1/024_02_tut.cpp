286c00c47f97f3bc81fef90921aac56cd6ffa735
Manas <gupta.manas6268@gmail.com>
2024-01-26 12:42:20 +0530

    Updated

---
+++ c++_language/Chapter 1/024_02_tut.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Employee{
+    int Id;
+    static int count ;
+
+
+    public:
+        void setData(void){
+            cout << "Enter the Id: ";
+            cin >> Id;
+            count++;
+        }
+        void getData(void){
+            cout << "The Id of this employee is: " << Id << 
+            " and this is employee number " << count << endl;
+        }
+        static void getCount(void){
+            // cout << Id; // Gives an errror.
+            cout << "The value of count is: " << count << endl;
+        }
+};
+
+int Employee :: count;
+
+int main(){
+    // See line-20. And also run this code when you 
+    // uncomment line-20 of Code.
+
+    Employee manas, rohan, elon;
+    
+    manas.setData();
+    manas.getData();
+    Employee::getCount();
+
+    rohan.setData();
+    rohan.getData();
+    Employee::getCount();
+
+    elon.setData();
+    elon.getData();
+    Employee::getCount();
+
+    return 0;
+}
