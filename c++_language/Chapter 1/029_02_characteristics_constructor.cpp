f2bdd5bebb1ac90f2cfe125a5f1ed1b083940c83
Manas <gupta.manas6268@gmail.com>
2024-01-26 18:04:26 +0530

    Updated

---
+++ c++_language/Chapter 1/029_02_characteristics_constructor.cpp
@@ -0,0 +1,17 @@
+// Characteristics of Constructors:
+// 
+// 1. It should be declared in the public section of the class.
+// 2. They are automatically invoked whenever the object is created.
+// 3. They cannot have return types and do not have return types.
+
+// 4. It can take default arguments(See default_values in page-13 of your notebook.)
+// 5. We cannot refer to their address
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
