e695b09fd5ccb03ae3b521ac6184cdf0d1f3f489
Manas <gupta.manas6268@gmail.com>
2024-01-28 00:14:25 +0530

    Updated

---
+++ c++_language/Chapter 1/036_Inheritance.cpp
@@ -0,0 +1,3 @@
+// There are Screenshot notes in this Lecture.
+
+// There is no Codes in this Lecture.
---
+++ c++_language/Chapter 1/037_01_tut.cpp
@@ -0,0 +1,66 @@
+// There is no Screenshot notes in this Lecture.
+
+// Run code and check OUTPUT of the code properly.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Base class
+class Employee{
+
+public:
+    int Id;
+    float salary;
+
+    Employee(int inputId){
+        Id = inputId;
+        salary = 34.0;
+    }
+    Employee(){}
+};
+
+// Derived Class syntax
+// class {{derived-class-name}} : {{visibility-mode}} {{base-class-name}}{
+
+//     class members/methods/etc...
+// }
+
+// Note:
+// 1. Default visibility mode is private
+// 2. Public Visibility Mode: Public members of the base class becomes Public members of
+//                              the derived class.  
+// 3. Private Visibility Mode: Public members of the base class becomes Private members of
+//                              the derived class.   
+// 4. Private members of the base class are never inherited.   
+
+
+// Creating a Programmer class derived from Employee Base class
+class Programmer : Employee{
+
+public:
+    Programmer(int inputId){
+        Id = inputId;  // Here, 'Id' has private visibility mode. So, it becomes a
+                       //  private member and is inaccessible from Outside.(See line-62 of code.)
+    }
+
+    int languageCode = 9;
+
+    void getData(){
+        cout << Id << endl;
+    }
+};
+
+int main(){
+    Employee manas(1), harry(2);
+
+    cout << manas.salary << endl;
+    cout << harry.salary << endl;
+    cout << endl;
+
+    Programmer skillF(10);
+    cout << skillF.languageCode << endl;
+    // cout << skillF.Id << endl;  // If we uncomment this line, then this code will give error.
+    skillF.getData();
+
+    return 0;
+}
