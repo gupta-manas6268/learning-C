afbeee3bc8b9f13b50e06618970f75e3e518fa6c
Manas <gupta.manas6268@gmail.com>
2024-02-01 21:54:56 +0530

    Updated

---
+++ c++_language/Chapter 1/054_01_tut.cpp
@@ -0,0 +1,23 @@
+// There is Screenshot notes in this Lecture.
+// There is Screenshot notes in this Lecture.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // Polymorphism -> One name & multiple forms
+    //  Example: Function overloading, Operator overloading,
+    //           Virtual Functions, etc.
+
+    // Polymorphism in C++ can be of two types:
+    // 1. Compile Time Polymorphism:
+    //      Compile Time Polymorphism in C++ is acheived using:
+    //      (a)- Function overloading
+    //      (b)- Operator overloading
+    // 
+    // 2. Run Time Polymorphism:
+    //      Run Time Polymorphism in C++ is acheived using:
+    //      (i)- Virtual functions
+
+    return 0;
+}
