c94bde96b35d92711647e4d3e5bf727606c48169
Manas <gupta.manas6268@gmail.com>
2024-02-10 18:22:04 +0530

    Updated

---
+++ c_language/Chapter 11/011_05_free.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int *ptr, *ptr2;
+    
+    ptr = (int *) malloc(600 * sizeof(int));
+
+    for(int i=0; i<600; i++){
+        ptr2 = (int *) malloc(600 * sizeof(int));
+        
+        printf("Enter the value of %d element is: ", i);
+        scanf("%d", &ptr[i]);
+
+        free(ptr2); // To deallocate/free memory of 'ptr2' malloc function.
+    }
+
+    for(int i=0; i<6; i++){
+        printf("The value of %d element is: %d\n", i, ptr[i]);
+    }
+
+    return 0;
+}
--- c_language/Chapter 11/011_06_realloc.c
+++ c_language/Chapter 11/011_06_realloc.c
@@ -3,11 +3,7 @@
 
 int main(){
     int *ptr;
-    // Sizeof operator in C
-    // printf("The size of int on my pc is %d\n", sizeof(int));
-    // printf("The size of float on my pc is %d\n", sizeof(float));
-    // printf("The size of char on my pc is %d\n", sizeof(char));
-
+    
     ptr = (int *) malloc(6 * sizeof(int));
 
     for(int i=0; i<6; i++){
@@ -16,6 +12,18 @@ int main(){
     }
 
     for(int i=0; i<6; i++){
+        printf("The value of %d element is: %d\n", i, ptr[i]);
+    }
+
+    // Reallocate ptr using realloc()
+    ptr = realloc(ptr, 10 * sizeof(int));
+
+    for(int i=0; i<10; i++){
+        printf("Enter the value of %d element is: ", i);
+        scanf("%d", &ptr[i]);
+    }
+
+    for(int i=0; i<10; i++){
         printf("The value of %d element is: %d\n", i, ptr[i]);
     }
 
