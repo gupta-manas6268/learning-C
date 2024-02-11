1ca4eb7fe682beca6427cc96cfee8eb63dd1742c
Manas <gupta.manas6268@gmail.com>
2024-02-11 13:12:59 +0530

    Updated

---
+++ Data_Structures/016_04_Insertion_after_node.c
@@ -0,0 +1,65 @@
+// Insertion after a node.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node * next;
+};
+
+void linkedListTraversal(struct Node *ptr){
+    while(ptr != NULL){
+        printf("Element: %d\n", ptr->data);
+        ptr = ptr->next;   
+    }
+}
+
+struct Node *insertAfterNode(struct Node *head, struct Node *previousNode, int data){
+    struct Node *ptr = (struct Node *) malloc(sizeof(struct Node));
+    ptr->data = data;
+
+    ptr->next = previousNode->next;
+    previousNode->next = ptr;
+
+    return head;
+}
+
+int main(){
+    struct Node * head;
+    struct Node * second;
+    struct Node * third;
+    struct Node * fourth;
+
+    // Allocate memory for nodes in the linked list in Heap.
+    head = (struct Node *) malloc(sizeof(struct Node));
+    second = (struct Node *) malloc(sizeof(struct Node));
+    third = (struct Node *) malloc(sizeof(struct Node));
+    fourth = (struct Node *) malloc(sizeof(struct Node));
+
+    // Link first and second nodes.
+    head->data = 7;
+    head->next = second;
+
+    // Link second and third nodes.
+    second->data = 11;
+    second->next = third;
+
+    // Link third and fourth nodes.
+    third->data = 66;
+    third->next = fourth;
+
+    // Terminate the list at the third node.
+    fourth->data = 41;
+    fourth->next = NULL;
+
+    printf("Linked list before insertion\n");
+    linkedListTraversal(head);
+    printf("\n");
+
+    printf("Linked list after insertion\n");
+    head = insertAfterNode(head, third, 45);
+    linkedListTraversal(head);
+
+    return 0;
+}
