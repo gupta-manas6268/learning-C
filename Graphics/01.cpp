2552cb4059500753e7af9590a0ddd4355240c2be
Manas <gupta.manas6268@gmail.com>
2024-02-11 17:59:22 +0530

    Updated

--- Graphics/01.cpp
+++ Graphics/01.cpp
@@ -1,7 +1,15 @@
-#include <stdio.h>
-#include <graphic.h>
+#include <GL/glut.h>
 
-int main(){
-    
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    // Your OpenGL drawing code here
+    glFlush();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutCreateWindow("OpenGL Example");
+    glutDisplayFunc(display);
+    glutMainLoop();
     return 0;
 }
