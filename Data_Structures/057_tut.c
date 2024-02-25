2569a928b997899ef00bc3e6645c0134b8be9278
Manas <gupta.manas6268@gmail.com>
2024-02-25 23:19:57 +0530

    Updated

---
+++ Data_Structures/057_tut.c
@@ -0,0 +1,3 @@
+// https://www.codewithharry.com/videos/data-structures-and-algorithms-in-hindi-57/
+
+// See 3-Screenshot notes.
---
+++ Data_Structures/058_tut.c
@@ -0,0 +1,5 @@
+// https://www.codewithharry.com/videos/data-structures-and-algorithms-in-hindi-58/
+
+// See 6-Screenshot notes.
+
+// (lo -> low), (hi -> high)
