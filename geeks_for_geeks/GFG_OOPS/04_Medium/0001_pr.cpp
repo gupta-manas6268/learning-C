77fdf959d6eb47841933de8728b7120b622f1b14
Manas <gupta.manas6268@gmail.com>
2024-03-23 23:33:00 +0530

    Updated

---
+++ geeks_for_geeks/GFG_OOPS/04_Medium/0001_pr.cpp
@@ -0,0 +1,42 @@
+// Longest Common Substring
+// https://www.geeksforgeeks.org/problems/longest-common-substring1452/1?page=1&category=Strings&difficulty=Medium&sortBy=submissions
+
+
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    
+    int longestCommonSubstr (string S1, string S2, int n, int m)
+    {
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                
+            }
+        }
+        // your code here
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n, m; cin >> n >> m;
+        string s1, s2;
+        cin >> s1 >> s2;
+        Solution ob;
+
+        cout << ob.longestCommonSubstr (s1, s2, n, m) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
