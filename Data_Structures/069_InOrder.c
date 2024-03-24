592db3dd3abd70a83e5387a1eaf6a33ba1951e98
Manas <gupta.manas6268@gmail.com>
2024-03-24 19:59:53 +0530

    Updated

--- Data_Structures/069_InOrder.c
+++ Data_Structures/069_InOrder.c
@@ -1,5 +1,3 @@
-// Linked representation Binary Tree.
-
 #include <stdio.h>
 #include <malloc.h>
 
@@ -19,19 +17,29 @@ struct node* createNode(int data){
     return n;
 }
 
+void inOrder(Node* root){
+    if(root != NULL){
+        inOrder(root->left);
+        printf("%d ", root->data);
+        inOrder(root->right);
+    }
+}
+
 int main(){
     // Constructing the root node = Using Function (Recommended)
-    Node *p = createNode(2);
-
-    // Constructing the second node.
+    Node *p = createNode(4);
     Node *p1 = createNode(1);
-
-    // Constructing the third node.
-    Node *p2 = createNode(4);
+    Node *p2 = createNode(6);
+    Node *p3 = createNode(5);
+    Node *p4 = createNode(2);
 
     // Linking the root node with left and right children.
     p->left = p1;
     p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    inOrder(p);
 
     return 0;
 }
