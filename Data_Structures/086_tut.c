5f38fc051d62a488e828a0a8a25ba6a9d3dda4ac
Manas <gupta.manas6268@gmail.com>
2024-04-10 14:36:07 +0530

    Updated

--- Data_Structures/086_tut.c
+++ Data_Structures/086_tut.c
@@ -1,1 +1,5 @@
+// From [03: 55] of this Lecture.
+
+
 // There is Screenshot notes in this Lecture.
+// (LOT -> Level Order Traversal.)
---
+++ TLE Eliminators/TLE Eliminators Level-2/Ch-001/22_pr.cpp
@@ -0,0 +1,10 @@
+// 78. Subsets
+
+// https://leetcode.com/problems/subsets/description/
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int Length = nums.length();
+    }
+};
