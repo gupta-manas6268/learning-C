7149948640a0c312f61c0f01d5d537752c7f1b56
Manas <gupta.manas6268@gmail.com>
2024-04-12 13:13:58 +0530

    Updated

---
+++ Web_Developnment_Project/Ch-015(Intermediate JavaScript)/002_index.js
@@ -0,0 +1,6 @@
+var a = prompt("What is your name: ");
+var b = prompt("What is your partner name: ");
+var n = (Math.random()) * 100;
+n = Math.floor(n) + 1;
+
+console.log(a + " weds " + b + " percentage of marriage is: " + n + "%.");
