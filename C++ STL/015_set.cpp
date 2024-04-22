8ef55b1be71c4255bb52584dcb667f237021c4b9
Manas <gupta.manas6268@gmail.com>
2024-04-23 00:05:07 +0530

    Updated

--- C++ STL/015_set.cpp
+++ C++ STL/015_set.cpp
@@ -8,18 +8,28 @@ void print(set <string> &s){
         cout << value << endl;
     }
     cout << endl;
-    for(auto it = s.begin(); it != s.end(); it++){
-        cout << (*it) << endl;
-    }
+    // Below loop gives same O/P as above loop.
+    
+    // for(auto it = s.begin(); it != s.end(); it++){  
+    //     cout << (*it) << endl;
+    // }
 }
 
 int main(){
     set <string> s;
     s.insert("abc");  // TC of insertion & access of element of set is = log(n).
+    s.insert("abc");  // See O/P of code carefully. & see that 'set' stores 
+                      //  unique values.
     s.insert("zsdf");
     s.insert("bcd");
 
     auto it = s.find("abc");  // it => iterator.
+    // auto it = s.find("abcd");  // TC of 's.find()' is = O(log(n)).
+    if(it != s.end()){
+        cout << (*it) << endl;
+        // s.erase(it);    // Also see O/P when uncommenting this line.
+    }
+    cout << endl;
     print(s);
 
     return 0;
