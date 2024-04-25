68a58eacdfab7f00568ca84123a486a3549417c7
Manas <gupta.manas6268@gmail.com>
2024-04-25 23:14:29 +0530

    Updated

--- TLE Eliminators/TLE eliminators Level-1/Chapter 9/212_pr.cpp
+++ TLE Eliminators/TLE eliminators Level-1/Chapter 9/212_pr.cpp
@@ -1,3 +1,47 @@
+// Correct.
+
 // 2784. Check if Array is Good
 // https://leetcode.com/problems/check-if-array-is-good/description/
 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
+
+
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int a = 1;
+
+        bool ans;
+        for(int i=0; i < nums.size(); i++){
+            if((i < (nums.size() - 1))){
+                if(nums[i] == a){
+                    a++;
+                }
+                else{
+                    ans = false;
+                    break;
+                }
+            }
+            else{
+                if(nums[i] == (nums.size() - 1)){
+                    ans = true;
+                    break;
+                }
+                else{
+                    ans = false;
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
