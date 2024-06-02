1970e51fee93265740c2709ab225865275a10587
Manas <gupta.manas6268@gmail.com>
2024-06-02 23:33:43 +0530

    Updated

---
+++ College_Dump/001_Virtual_Function.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Shape{
+public:
+    virtual double area() const = 0;
+    virtual ~Shape(){}
+};
+
+class Circle: public Shape{
+// private:
+//     double radius;
+// public:
+    // Circle(double r): radius(r){}
+    // double 
+};
+
+int main(){
+    
+    return 0;
+}
