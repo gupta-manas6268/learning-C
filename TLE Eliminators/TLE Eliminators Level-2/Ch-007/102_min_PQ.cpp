697f0e047a181c06e56dc36aa58b38fba19e4348
Manas <gupta.manas6268@gmail.com>
2024-06-14 19:47:35 +0530

    Updated

---
+++ TLE Eliminators/TLE Eliminators Level-2/Ch-007/102_min_PQ.cpp
@@ -0,0 +1,25 @@
+// max PQ -> max Priority Queue.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long 
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+signed main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    // Max Priority Queue.
+    priority_queue<int> pq;
+    // It arranges elements in a decreasing-order in which
+    //  top element is the largest element.
+    pq.push(9);
+    pq.push(5);
+    pq.push(11);
+    cout << pq.top() << endl;
+    pq.pop();
+    cout << pq.top() << endl;
+}
