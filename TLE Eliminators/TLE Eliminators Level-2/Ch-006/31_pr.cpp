c6205180acd76ee60f33b4b066bd1a088d436fc3
Manas <gupta.manas6268@gmail.com>
2024-06-18 11:27:02 +0530

    Updated

---
+++ TLE Eliminators/TLE Eliminators Level-2/Ch-006/31_pr.cpp
@@ -0,0 +1,19 @@
+// 
+
+// 372. Super Pow
+// https://leetcode.com/problems/super-pow/description/
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    
+    return 0;
+}
--- TLE Eliminators/TLE Eliminators Level-2/Ch-006/32_pr.cpp
+++ TLE Eliminators/TLE Eliminators Level-2/Ch-006/32_pr.cpp
@@ -14,13 +14,41 @@ using namespace std;
 const int MOD = 1e9 + 7;
 const int INF = LLONG_MAX >> 1;
 
-int fact(int n){
-    int ans = 1;
+long long fact(int n){
+    long long ans = 1;
     for(int i=1; i<=n; i++){
         ans *= i;
     }
 
     return ans;
+}
+
+long long factor(long long n){
+    long long ans = 2;
+    for(long long i=2; i*i <= n; i++){
+        if(n%i == 0){
+            if(i != (n/i)){ ans += 2;}
+            else{ ans += 1;}
+        }
+    }
+    return ans;
+}
+
+vector<int> Factor(int n){
+    vector<int> facts;
+    for(int i=2; i*i <= n; i++){
+        if(n%i == 0){
+            if(i != (n/i)){
+                facts.push_back(i);
+                facts.push_back(n/i);
+            }
+            else{
+                facts.push_back(i);
+            }
+        }
+    }
+    sort(facts.begin(), facts.end());
+    return facts;
 }
 
 signed main(){
@@ -32,6 +60,12 @@ signed main(){
     ios::sync_with_stdio(false); cin.tie(NULL);
 
     int n; cin >> n;
-    int ans = fact(n) % MOD;
-    cout << ans << endl;
+    vector<int> Ans = Factor(n);
+    for(int i=0; i<Ans.size()-1; i++){
+        cout << Ans[i] << endl;
+    }
+    // int number = fact(n);
+    // cout << number << endl;
+    // int ans = factor(number) % MOD;
+    // cout << ans << endl;
 }
