75a2d722d9a44fa8a6694c2c79fc7c08c39edd67
Manas <gupta.manas6268@gmail.com>
2024-06-21 14:38:32 +0530

    Updated

--- TLE Eliminators/TLE Eliminators Level-2/Ch-007/0_41_01_pr.cpp
+++ TLE Eliminators/TLE Eliminators Level-2/Ch-007/0_41_01_pr.cpp
@@ -1,7 +1,22 @@
-// 
+// Wrong.
 
 // 150. Evaluate Reverse Polish Notation
 // https://leetcode.com/problems/evaluate-reverse-polish-notation/description/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- TLE Eliminators/TLE Eliminators Level-2/Ch-007/0_41_02_pr.cpp
+++ TLE Eliminators/TLE Eliminators Level-2/Ch-007/0_41_02_pr.cpp
@@ -1,12 +1,74 @@
-// 
+// Wrong (Runtime error)
 
 // 150. Evaluate Reverse Polish Notation
 // https://leetcode.com/problems/evaluate-reverse-polish-notation/description/
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
+
+void evalRPN(vector<string>& tokens) {
+    stack<int> st;
+
+    for(int i=0; i < tokens.size(); i++){
+        if((tokens[i] != "+") || (tokens[i] != "-") || (tokens[i] != "*") || (tokens[i] != "/")){
+            int a = 0;
+            bool negative = false;
+            for(int j=0; j < tokens[i].size(); j++){
+                if(tokens[i] == "-"){
+                    negative = true;
+                }
+                else{
+                    a = (10 * a) + (int)(tokens[i][j]);
+                }
+            }
+            if(negative == true){ a = -a;}
+            cout << a << endl;
+            st.push(a);
+        }
+        else{
+            int a, b;
+            st.top() = b;
+            st.pop();
+            st.top() = a;
+            st.pop();
+
+            if(tokens[i] == "+"){
+                int c = a + b;  st.push(c);
+            }
+            else if(tokens[i] == "-"){
+                int c = a - b;  st.push(c);
+            }
+            else if(tokens[i] == "*"){
+                int c = a * b;  st.push(c);
+            }
+            else{
+                int c = (a / b);  st.push(c);
+            }
+        }
+    }
+
+    int ans = st.top();
+    cout << endl << ans << endl;
+}
 
 int main(){
     #ifndef ONLINE_JUDGE
@@ -14,6 +76,14 @@ int main(){
     freopen("output.txt", "w", stdout);
     #endif
 
+    int n; cin >> n;
+    vector<string> a;
+    for(int i=0; i<n; i++){
+        string A; cin >> A;
+        a.push_back(A);
+    }
+
+    evalRPN(a);
     
     return 0;
 }
@@ -21,7 +91,5 @@ int main(){
 
 class Solution {
 public:
-    int evalRPN(vector<string>& tokens) {
-        
-    }
+    
 };
