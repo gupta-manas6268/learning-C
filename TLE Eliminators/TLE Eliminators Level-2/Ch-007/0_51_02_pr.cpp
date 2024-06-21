96f9376db4632a025c3a7ec65640831fa9d44b78
Manas <gupta.manas6268@gmail.com>
2024-06-21 14:44:12 +0530

    Updated

---
+++ TLE Eliminators/TLE Eliminators Level-2/Ch-007/0_51_02_pr.cpp
@@ -0,0 +1,30 @@
+// 
+
+// Matched Brackets 2
+// https://www.codechef.com/practice/course/zco-inoi-problems/ZCOPRAC/problems/ZCO12003
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long 
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+signed main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    int n; cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++){ cin >> arr[i];}
+
+    
+}
