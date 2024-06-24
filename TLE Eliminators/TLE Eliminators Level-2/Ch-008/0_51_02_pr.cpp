decedc6428083ab50543d94774f4b8f9201bf2fc
Manas <gupta.manas6268@gmail.com>
2024-06-24 14:26:15 +0530

    Updated

---
+++ TLE Eliminators/TLE Eliminators Level-2/Ch-008/0_51_02_pr.cpp
@@ -0,0 +1,26 @@
+// 
+
+// D - Five, Five Everywhere
+// https://atcoder.jp/contests/abc096/tasks/abc096_d
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long 
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+signed main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    
+}
