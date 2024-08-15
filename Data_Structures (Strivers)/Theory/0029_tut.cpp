70c9d403a1faf535bab728d8a28b7679a1335a2e
Manas <gupta.manas6268@gmail.com>
2024-08-15 23:36:33 +0530

    Updated

---
+++ Data_Structures (Strivers)/Theory/0029_tut.cpp
@@ -0,0 +1,1 @@
+// From this Lecture.
--- TLE Eliminators/TLE Eliminators Level-3/Ch-002/104_Quiz.cpp
+++ TLE Eliminators/TLE Eliminators Level-3/Ch-002/104_Quiz.cpp
@@ -14,6 +14,65 @@ using namespace std;
 const int MOD = 1e9 + 7;
 const int INF = LLONG_MAX >> 1;
 
+// [10, 20, 10, 10, 10, 30]
+struct Interactor{
+    long long n;
+    int majority_Element = -1;
+    vector<int> arr;
+
+    Interactor(long long n1){
+        n = n1;
+        vector<int> arr = {10, 20, 10, 30, 10, 10, 10};
+        majority_Element = 10;
+    }
+    string queryInteractor(long long index){
+        if(arr[index - 1] == majority_Element){
+            return "Yes";
+        }
+        else{
+            return "No";
+        }
+    }
+};
+
+bool testing = true;
+
+string query(long long index, Interactor &it){
+    cout << "? " << index << endl;
+
+    // Interactor it;
+    string output; 
+
+    if(testing){
+        output = it.queryInteractor(index);
+        cout << "Interactor Output: " << output << endl;
+    }
+    else{
+        cin >> output;
+    }
+    return output;
+}
+
+void solve(){
+    long long n; cin >> n;
+
+    Interactor it = Interactor(n);
+
+    int iterations = 20;
+    long long ans = -1;
+    for(int i=0; i < iterations; i++){
+        // int x = getRandomNumber(1, n);
+        int x = (rand() % n) + 1;
+
+        string result = query(x, it);
+        if(result == "Yes"){
+            ans = x;
+            break;
+        }
+    }
+    cout << "! " << ans << endl;
+}
+
 signed main(){
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
@@ -22,5 +81,5 @@ signed main(){
 
     ios::sync_with_stdio(false); cin.tie(NULL);
 
-    int n;
+    solve();
 }
