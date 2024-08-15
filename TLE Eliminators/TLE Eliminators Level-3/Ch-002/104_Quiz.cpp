8ce6a71f5bc0d74396442c30ae0940a38a173779
Manas <gupta.manas6268@gmail.com>
2024-08-15 16:10:41 +0530

    Updated

---
+++ TLE Eliminators/TLE Eliminators Level-3/Ch-002/104_Quiz.cpp
@@ -0,0 +1,26 @@
+// Quiz: There is a hidden array of N integers, you need to find any index of the majority
+//        element or report that no such element exists. You can ask for any index and
+//        the computer will tell you if that index contains the majority element or not.
+//        Constraints: 1 <= N <= 1018.
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long 
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+signed main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    int n;
+}
