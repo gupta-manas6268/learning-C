e698198e69b03ba5968d7dfce64b21be45efdda9
Manas <gupta.manas6268@gmail.com>
2024-08-24 14:25:48 +0530

    Updated

--- TLE Eliminators/TLE Eliminators Level-2/Ch-010/0_21_01_pr.cpp
+++ TLE Eliminators/TLE Eliminators Level-2/Ch-010/0_21_01_pr.cpp
@@ -5,6 +5,35 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
--- TLE Eliminators/TLE Eliminators Level-2/Ch-010/0_21_02_pr.cpp
+++ TLE Eliminators/TLE Eliminators Level-2/Ch-010/0_21_02_pr.cpp
@@ -1,7 +1,40 @@
-// Wrong.
+// This is Mentor's code.
+// Correct.
 
 // D - Lazy Faith
 // https://atcoder.jp/contests/abc119/tasks/abc119_d
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -22,5 +55,45 @@ signed main(){
 
     ios::sync_with_stdio(false); cin.tie(NULL);
 
-    
+    long long a, b, q; cin >> a >> b >> q;
+    vector<long long> s(a + 2), t(b + 2);
+    for(long long i=1; i <= a; i++){ 
+        cin >> s[i];
+    }
+    for(long long i=1; i <= b; i++){
+        cin >> t[i];
+    }
+
+    s[0] = t[0] = -1e18;        // These two lines are just 
+    s[a + 1] = t[b + 1] = 1e18; //  assuming.
+
+    while(q--){
+        long long x; cin >> x;
+
+        long long ans = 1e18;
+
+        long long left_shrine = *(--upper_bound(s.begin(), s.end(), x));
+        long long left_temple = *(--upper_bound(t.begin(), t.end(), x));
+        long long right_shrine = *lower_bound(s.begin(), s.end(), x);
+        long long right_temple = *lower_bound(t.begin(),t.end(), x);
+
+        // Left shrine, Left temple
+        ans = min(ans, x - min(left_shrine, left_temple));
+
+        // Left shrine, Right temple
+        ans = min(ans, right_temple - left_shrine + min(x - left_shrine, right_temple - x));
+
+        // Right shrine, Left temple
+        ans = min(ans, right_shrine - left_temple + min(right_shrine - x, x - left_temple));
+
+        // Right shrine, Right temple
+        ans = min(ans, max(right_shrine, right_temple) - x);
+
+        cout << ans << endl;
+
+        // n => a, b
+
+        // TC = O(q * log(n)).
+        // SC = O(n).
+    }
 }
