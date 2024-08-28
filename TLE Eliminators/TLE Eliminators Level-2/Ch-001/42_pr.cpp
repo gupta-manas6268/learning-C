956f99292273f3dd319d5531a5eaa0dd9a13c1a7
Manas <gupta.manas6268@gmail.com>
2024-08-28 23:30:28 +0530

    Updated

---
+++ TLE Eliminators/TLE Eliminators Level-2/Ch-001/42_pr.cpp
@@ -0,0 +1,34 @@
+// 
+
+// V. Creating Expression1
+// https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/V
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long 
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+int solve(int a[], int n, int i){
+    
+}
+
+signed main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    int N, X; cin >> N >> X;
+    int A[N];
+    for(int i=0; i<N; i++){ cin >> A[i];}
+
+
+}
