fca3226f409daaa432dc2cd5bcd4485bdda95c38
Manas <gupta.manas6268@gmail.com>
2024-08-30 23:18:47 +0530

    Updated

---
+++ TLE Eliminators/TLE Eliminators Level-3/Ch-004/11_pr.cpp
@@ -0,0 +1,65 @@
+// 
+
+// C. Number of Equal
+// https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/C
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long 
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+signed main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    int n, m; cin >> n >> m;
+    vector<int> a, b;
+    for(int i=0; i<n; i++){
+        int temp; cin >> temp;
+        a.push_back(temp);
+    }
+    for(int i=0; i<m; i++){
+        int temp; cin >> temp;
+        b.push_back(temp);
+    }
+
+    int ans = 0;
+    int i = 0, j = 0;
+    while((i < n) && (j < m)){
+        if(a[i] != b[j]){
+            if(a[i] < b[j]){
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+        else{
+            int m = 0, n = 0;
+            for(int x = i; x < n; x++){
+                if(a[x] == a[i]){ m++;}
+                else{ i = (x-1); break;}
+            }
+            for(int x = j; x < m; x++){
+                if(b[x] == b[j]){ n++;}
+                else{ j = (x-1); break;}
+            }
+
+            ans += (n * m);
+            i++; j++;
+        }
+    }
+
+    cout << ans << endl;
+    // TC = O(n + m).
+}
