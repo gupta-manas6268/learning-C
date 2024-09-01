a9b7dcdba7ab89dfe63a375b86be1418887e52b2
Manas <gupta.manas6268@gmail.com>
2024-09-01 23:21:42 +0530

    Updated

--- TLE Eliminators/TLE Eliminators Level-2/Ch-001/004_Quiz.cpp
+++ TLE Eliminators/TLE Eliminators Level-2/Ch-001/004_Quiz.cpp
@@ -19,40 +19,49 @@ int main(){
 }
 
 
-class Solution {
-public:
-    int ans = 0;
-    void func(int n){
-        if(n == 0){ ans++;}
-        else if(n == 1){ func(n-1);}
-        else{
-            func(n-1);
-            func(n-2);
-        }
-    }
-    int climbStairs(int n) {
-        func(n);
-        return ans;
-    }
-};
+// class Solution {
+// public:
+//     int ans = 0;
+//     void func(int n){
+//         if(n == 0){ ans++;}
+//         else if(n == 1){ func(n-1);}
+//         else{
+//             func(n-1);
+//             func(n-2);
+//         }
+//     }
+//     int climbStairs(int n) {
+//         func(n);
+//         return ans;
+//     }
+// };
 
 
 class Solution {
 public:
-    long long int fact(long long int n){
-        // return n * fact(n-1);
-        int ans = 1;
-        for(int i=1; i<=n; i++){
-            ans *= i;
+    // long long int fact(long long int n){
+    //     // return n * fact(n-1);
+    //     int ans = 1;
+    //     for(int i=1; i<=n; i++){
+    //         ans *= i;
+    //     }
+    //     return ans;
+    // }
+    int nCr(int n, int r){
+        // long long int Ans = (fact(n)/ (fact(n-r) * fact(r)));
+        int maxi = max(r, n-r);
+        int mini = min(r, n-r);
+        int Ans = 1;
+
+        int j = 1;
+        for(int i = (maxi+1); i <= n; i++){
+            Ans *= (i/j);
+            j++;
         }
-        return ans;
-    }
-    long long int nCr(long long int n, long long int r){
-        long long int Ans = (fact(n)/ (fact(n-r) * fact(r)));
         return Ans;
     }
-    long long int climbStairs(long long int n) {
-        long long int ans = 0;
+    int climbStairs(int n) {
+        int ans = 0;
         for(int i=n; i>0; i++){
             if(i < (n-i)){ break;}
             if(i == n){ ans++;}
---
+++ TLE Eliminators/TLE Eliminators Level-2/Ch-001/005_Quiz.cpp
@@ -0,0 +1,5 @@
+// https://codeforces.com/blog/entry/92031
+
+// Here, Except Problem-1 you have solve all Questions.
+
+// Problem-1 ==> (004_Quiz.cpp).
