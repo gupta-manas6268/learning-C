f22e02394b535c080fc3d82535c642a1b564eb9f
Manas <gupta.manas6268@gmail.com>
2024-09-04 12:55:59 +0530

    Updated

--- TLE Eliminators/TLE Eliminators Level-2/Ch-005/03_Sieve_Prime_Factorisation.cpp
+++ TLE Eliminators/TLE Eliminators Level-2/Ch-005/03_Sieve_Prime_Factorisation.cpp
@@ -50,5 +50,10 @@ void solve(){
 signed main(){
     ios::sync_with_stdio(false); cin.tie(NULL);
 
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
     solve();
 }
