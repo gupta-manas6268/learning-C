9c8863dc412a6f572a3f8a17b17eee52f8ee3437
Manas <gupta.manas6268@gmail.com>
2024-09-16 14:56:05 +0530

    Updated

--- C++ STL/024_Inbuilt_Sort.cpp
+++ C++ STL/024_Inbuilt_Sort.cpp
@@ -4,6 +4,11 @@
 using namespace std;
 
 int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
     int n; cin >> n;
     int a[n];
     for(int i=0; i<n; i++){
