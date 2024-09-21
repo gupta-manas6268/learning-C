e7685881b99f70ba62232739e17bb6886ac41717
Manas <gupta.manas6268@gmail.com>
2024-09-21 12:35:13 +0530

    Updated

---
+++ TLE Eliminators/TLE Eliminators Level-2/Ch-004/002_Count.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long 
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+void solve(){
+    int a = 924568;
+    int ans = __builtin_popcountll(a);
+    // __builtin_popcountll(a) => It counts all the set(1)-bits
+    //                             of integer.
+    cout << ans << endl;
+}
+
+signed main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    solve();
+}
