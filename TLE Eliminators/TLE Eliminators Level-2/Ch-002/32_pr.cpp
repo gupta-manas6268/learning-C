4c3a627bb2861fadd6a9850f383d89a75d8692e4
Manas <gupta.manas6268@gmail.com>
2024-10-02 17:08:56 +0530

    Updated

---
+++ TLE Eliminators/TLE Eliminators Level-2/Ch-002/32_pr.cpp
@@ -0,0 +1,39 @@
+// 
+
+// 22. Generate Parentheses
+// https://leetcode.com/problems/generate-parentheses/description/
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    
+    return 0;
+}
+
+
+
+class Solution {
+public:
+    vector<string> ans;
+
+    void solve(string s, int n){
+        if(s.size() == (2 * n)){
+            ans.push_back(s);
+            return;
+        }
+
+        
+    }
+
+    vector<string> generateParenthesis(int n) {
+        
+    }
+};
