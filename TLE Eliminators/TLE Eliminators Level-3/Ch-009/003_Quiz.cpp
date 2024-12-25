cb9f9e67ebe4f704811239bc582e7834a5ca6d3b
Manas <gupta.manas6268@gmail.com>
2024-12-26 00:20:00 +0530

    Updated

---
+++ TLE Eliminators/TLE Eliminators Level-3/Ch-009/003_Quiz.cpp
@@ -0,0 +1,13 @@
+// Problem-2: Palindrome Queries
+//             Calculate no. of different substrings 
+//             which are Palindrome.
+// Ans=> See Slide-1, Page-32 to 33.
+//        TC = O(n).
+
+
+// This is a H.W. Question. Solve it Yourself.
+
+// 
+
+// Palindrome Queries
+// https://cses.fi/problemset/task/2420
---
+++ TLE Eliminators/TLE Eliminators Level-3/Ch-009/004_Quiz.cpp
@@ -0,0 +1,17 @@
+// Problem-3: Largest substring which repeats atleast
+//             twice.
+// Ans=> See Slide-1, Page-34 to 37.
+//      By using Custom Unordered-Map, TC = O(n*log(n)).
+//   See 'Custom Unordered-Map' on Internet for better
+//    understanding.
+// 
+//   'Custom Unordered-Map' has its own 'Hash' table which
+//    which reduces Collision by a lot.
+
+
+// This is a H.W. Question. Solve it Yourself.
+
+// 
+
+// 1044. Longest Duplicate Substring
+// https://leetcode.com/problems/longest-duplicate-substring/description/
