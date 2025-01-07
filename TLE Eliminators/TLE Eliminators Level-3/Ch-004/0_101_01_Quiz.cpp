415a366c99029d6c13f1cd3947d85d64bb02d706
Manas <gupta.manas6268@gmail.com>
2025-01-07 21:10:17 +0530

    Updated

--- TLE Eliminators/TLE Eliminators Level-3/Ch-004/0_101_01_Quiz.cpp
+++ TLE Eliminators/TLE Eliminators Level-3/Ch-004/0_101_01_Quiz.cpp
@@ -1,7 +1,40 @@
-// 
+// Wrong.
 
 // 862. Shortest Subarray with Sum at Least K
 // https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/description/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- TLE Eliminators/TLE Eliminators Level-3/Ch-004/100_tut.cpp
+++ TLE Eliminators/TLE Eliminators Level-3/Ch-004/100_tut.cpp
@@ -1,1 +1,6 @@
 // This is Part-02 of this Lecture.
+
+// In Slide-02, Page-09, (inc order => increasing order.)
+// (p => prefix_sum.)
+
+// Page-10, (pq => priority_queue.)
