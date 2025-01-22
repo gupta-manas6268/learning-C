16beb40899595063a3ab95b48984995a5682f3d7
Manas <gupta.manas6268@gmail.com>
2025-01-22 12:55:47 +0530

    Updated

--- TLE Eliminators/TLE Eliminators Level-3/Ch-002/001_Quiz.cpp
+++ TLE Eliminators/TLE Eliminators Level-3/Ch-002/001_Quiz.cpp
@@ -22,9 +22,50 @@ signed main(){
 
     ios::sync_with_stdio(false); cin.tie(NULL);
 
-    int tc; cin >> tc;
+    // I/P
+    int n; cin >> n;
+    vector<int> A(n);
+    for(int i=0; i<n; i++){ cin >> A[i];}
 
-    while (tc--){
-        
+    // O/P
+    double left = *min(A.begin(), A.end());
+    double right = *max(A.begin(), A.end());
+
+    double precision = 1e-4;
+    double avg, median;
+    // int count = 0;
+    // while((right - left) > precision){
+    for(int count = 0; count <= 100; count++){
+        double mid = (left + right)/2;
+
+        vector<double> B(n);
+        for(int i=0; i<n; i++){
+            B[i] = ((double)(A[i]) - mid);
+        }
+        vector<double> C(n);
+        for(int i=0; i<n; i++){
+            if(i == 0){ C[0] = B[0];}
+            else if(i == 1){
+                double temp1 = 0;
+                C[i] = (B[i] + max(C[i-1], temp1));
+            }
+            else if(i >= 2){
+                C[i] = (B[i] + max(C[i-1], C[i-2]));
+            }
+        }
+
+        double sum = max(C[n-1], C[n-2]);
+        if(sum >= 0){
+            avg = mid;
+            left = (mid + precision);
+        }
+        else{
+            right = (mid - precision);
+        }
+
+        // count++;
+        // if(count > 100){ break;}
     }
+
+    cout << avg << endl;
 }
