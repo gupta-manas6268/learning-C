9a78fa231433c30a2488688cbf84d9f4cce29b78
Manas <gupta.manas6268@gmail.com>
2025-02-26 00:06:16 +0530

    Updated

---
+++ TLE Eliminators/TLE Eliminators Level-3/Ch-002/42_pr.cpp
@@ -0,0 +1,7 @@
+// 
+
+// D. Fixed Point Guessing
+// https://codeforces.com/problemset/problem/1698/D
+
+
+
--- TLE Eliminators/TLE Eliminators Level-3/Ch-005/001_tut.cpp
+++ TLE Eliminators/TLE Eliminators Level-3/Ch-005/001_tut.cpp
@@ -1,5 +1,3 @@
-// Aloo Bhujiya
-
 // From [01:44:00] of 'Number Theory-2' Lecture.
 
 // Slide-34 => (relatively prime => coprime.)
