882347b008f570da670b61885a6f8ca870cf8648
Manas <gupta.manas6268@gmail.com>
2025-03-21 00:09:14 +0530

    Updated

---
+++ TLE Eliminators/TLE Eliminators Level-3/Ch-008/022_Quiz.cpp
@@ -0,0 +1,31 @@
+// From [39:00] of Lecture-2.
+// 
+
+// D. Bracket Coloring
+// https://codeforces.com/contest/1837/problem/D
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long 
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+signed main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    int tc; cin >> tc;
+
+    while (tc--){
+        
+    }
+}
