17abc24e0a26ce49d331e280fbbdfb145243cbbd
Manas <gupta.manas6268@gmail.com>
2025-04-20 23:57:57 +0530

    Updated

--- TLE Eliminators/TLE Eliminators Level-3/Ch-008/042_Quiz.cpp
+++ TLE Eliminators/TLE Eliminators Level-3/Ch-008/042_Quiz.cpp
@@ -1,1 +1,8 @@
-// From [1:02:00] of this Lecture-4.
+// 
+
+// Firstly by Seeing Slide Concepts, try your own code.
+//  & if you didn't solve then see Mentor's code in 
+//  Slide-4, Page-19, in https://codeforces.com/contest/1949/submission/285211553
+
+// B. Charming Meals
+// https://codeforces.com/problemset/problem/1949/B
---
+++ TLE Eliminators/TLE Eliminators Level-3/Ch-008/043_Quiz.cpp
@@ -0,0 +1,6 @@
+// This is H.W. Problem.
+
+// 
+
+// C. Zero-Sum Prefixes
+// https://codeforces.com/problemset/problem/1748/C
