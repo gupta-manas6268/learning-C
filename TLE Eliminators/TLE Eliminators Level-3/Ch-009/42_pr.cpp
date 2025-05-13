31b88e98b172be2e54eb0a41136fbd85d7d05927
Manas <gupta.manas6268@gmail.com>
2025-05-13 20:44:16 +0530

    Updated

---
+++ TLE Eliminators/TLE Eliminators Level-3/Ch-009/42_pr.cpp
@@ -0,0 +1,162 @@
+// Correct.
+
+// B. Password
+// https://codeforces.com/problemset/problem/126/B
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long 
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+// This is Github, "String-Hashing" template.
+const int N = 1e6 + 9;
+
+int power(long long n, long long k, const int mod) {
+  int ans = 1 % mod;
+  n %= mod;
+  if (n < 0) n += mod;
+  while (k) {
+    if (k & 1) ans = (long long) ans * n % mod;
+    n = (long long) n * n % mod;
+    k >>= 1;
+  }
+  return ans;
+}
+
+const int MOD1 = 127657753, MOD2 = 987654319;
+const int p1 = 137, p2 = 277;
+int ip1, ip2;
+pair<int, int> pw[N], ipw[N];
+
+void prec() {
+  pw[0] =  {1, 1};
+  for (int i = 1; i < N; i++) {
+    pw[i].first = 1LL * pw[i - 1].first * p1 % MOD1;
+    pw[i].second = 1LL * pw[i - 1].second * p2 % MOD2;
+  }
+
+  ip1 = power(p1, MOD1 - 2, MOD1);
+  ip2 = power(p2, MOD2 - 2, MOD2);
+  ipw[0] =  {1, 1};
+  for (int i = 1; i < N; i++) {
+    ipw[i].first = 1LL * ipw[i - 1].first * ip1 % MOD1;
+    ipw[i].second = 1LL * ipw[i - 1].second * ip2 % MOD2;
+  }
+
+}
+struct Hashing {
+  int n;
+  string s; // 0 - indexed
+  vector<pair<int, int>> hs; // 1 - indexed
+
+  Hashing() {}
+  Hashing(string _s) {
+    n = _s.size();
+    s = _s;
+    hs.emplace_back(0, 0);
+    for (int i = 0; i < n; i++) {
+      pair<int, int> p;
+      p.first = (hs[i].first + 1LL * pw[i].first * s[i] % MOD1) % MOD1;
+      p.second = (hs[i].second + 1LL * pw[i].second * s[i] % MOD2) % MOD2;
+      hs.push_back(p);
+    }
+  }
+
+  pair<int, int> get_hash(int l, int r) { // 1 - indexed
+    assert(1 <= l && l <= r && r <= n);
+    pair<int, int> ans;
+    ans.first = (hs[r].first - hs[l - 1].first + MOD1) * 1LL * ipw[l - 1].first % MOD1;
+    ans.second = (hs[r].second - hs[l - 1].second + MOD2) * 1LL * ipw[l - 1].second % MOD2;
+    return ans;
+  }
+
+  pair<int, int> get_hash() {
+    return get_hash(1, n);
+  }
+};
+// Github, "String-Hashing" template end.
+
+signed main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    prec();
+    // I/P
+    string s; cin >> s;
+
+    // Solution
+    int n = s.length();
+    struct Hashing h1 = Hashing(s);
+    string ans = "Just a legend";
+    if(n >= 3){
+        for(int size = n-2; size >= 1; size--){
+            pair<int,int> A = h1.get_hash(1, size);
+            pair<int,int> B = h1.get_hash(n-size+1, n);
+            bool Break = false;
+
+            if((A.first == B.first) && (A.second == B.second)){
+                for(int l=2; l <= n-size; l++){
+                    int r = (size+l-1);
+                    pair<int,int> C = h1.get_hash(l, r);
+
+                    if((A.first == C.first) && (A.second == C.second)){
+                        string temp = "";
+                        for(int i=0; i < size; i++){
+                            temp += s[i];
+                        }
+                        ans = temp;
+                        Break = true;
+                        break;
+                    }
+                }
+            }
+            if(Break == true){ break;}
+        }
+    }
+
+    // O/P
+    cout << ans << endl;
+}
