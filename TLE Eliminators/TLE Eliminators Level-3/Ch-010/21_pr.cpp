502c98ab3634f8aa7ef55b78827c74d05215d69d
Manas <gupta.manas6268@gmail.com>
2025-05-21 16:59:21 +0530

    Updated

--- TLE Eliminators/TLE Eliminators Level-3/Ch-010/21_pr.cpp
+++ TLE Eliminators/TLE Eliminators Level-3/Ch-010/21_pr.cpp
@@ -1,36 +1,83 @@
+// Correct.
+
+// Shortest Unique prefix for every word
+// https://www.geeksforgeeks.org/problems/shortest-unique-prefix-for-every-word/1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
-#define endl '\n'
-#define int long long 
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
 
-const int MOD = 1e9 + 7;
-const int INF = LLONG_MAX >> 1;
+    
+    return 0;
+}
 
-const int letters = 26;
-struct Node{
-    vector<int> children;
-    int string_ending_here = 0;
-    int string_going_below = 0;
-    Node(){
-        children.resize(letters);
-        fill(begin(children), end(children), -1);
-    }
-};
 
-struct Trie{
+
+class Trie {
+public:
+    const int letters = 26;
+    struct Node{
+        vector<int> children;
+        int string_ending_here = 0;
+        int string_going_below = 0;
+        Node(){
+            children.resize(26);
+            fill(begin(children), end(children), -1);
+        }
+    };
+
     vector<Node> trie_tree;
     int size_Of_Trie = 0;
-    Trie(){
+    Trie() {
         trie_tree.emplace_back();
         // This creates a new node & pushes at the 
         // back of the current vector.
         size_Of_Trie++;
     }
-
-    void add_String(string &x){
+    
+    void insert(string word) {
         int current_node = 0;
-        for(char c : x){
+        for(char c : word){
             int index = c - 'a';
             if(trie_tree[current_node].children[index] == -1){
                 trie_tree[current_node].children[index] = size_Of_Trie;
@@ -43,9 +90,9 @@ struct Trie{
         trie_tree[current_node].string_ending_here++; 
     }
 
-    bool Search_String(string &x){
+    bool search(string word) {
         int current_node = 0;
-        for(char c : x){
+        for(char c : word){
             int index = c - 'a';
             if(trie_tree[current_node].children[index] == -1){
                 return false;
@@ -54,36 +101,8 @@ struct Trie{
         }
         return trie_tree[current_node].string_ending_here > 0;
     }
-
-    void Delete_Unused_Nodes(vector<pair<int,int>> nodes){
-        for(auto node : nodes){
-            Node here = trie_tree[node.first];
-            Node child = trie_tree[here.children[node.second]];
-            if(child.string_going_below == 0){
-                // for this code, the child at index node.second has not
-                trie_tree[node.first].children[node.second] = -1;
-            }
-        }
-    }
-
-    void Delete_String(string &x){
-        if(Search_String(x) != true){ return;}
-
-        int current_node = 0;
-        vector<pair<int,int>> Encountered_Nodes;
-        for(char c : x){
-            int index = c - 'a';
-            int parent = current_node;
-            
-            trie_tree[current_node].string_going_below--;
-            current_node = trie_tree[current_node].children[index];
-            
-            Encountered_Nodes.push_back({parent, index});
-        }
-        trie_tree[current_node].string_ending_here--;
-    }
-
-    bool Search_Prefix(string &prefix) {
+    
+    bool startsWith(string prefix) {
         int current_node = 0;
         for(char c : prefix){
             int index = c - 'a';
@@ -95,7 +114,7 @@ struct Trie{
         return (trie_tree[current_node].string_going_below > 0) || (trie_tree[current_node].string_ending_here > 0);
     }
 
-    int noOfPrefixes(string &prefix){
+    int noOfPrefixes(string prefix){
         int current_node = 0;
         int ans = 0;
         for(char c : prefix){
@@ -109,7 +128,7 @@ struct Trie{
         return ans;
     }
 
-    int string_Ending(string &prefix){
+    int string_Ending(string prefix){
         int current_node = 0;
         int ans = 0;
         for(char c : prefix){
@@ -124,18 +143,29 @@ struct Trie{
     }
 };
 
-signed main(){
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
+class Solution {
+public:
+    vector<string> findPrefixes(string arr[], int n){
+        Trie T = Trie();
+        for(int i=0; i<n; i++){
+            T.insert(arr[i]);
+        }
 
-    ios::sync_with_stdio(false); cin.tie(NULL);
+        vector<string> ans;
+        for(int i=0; i<n; i++){
+            int size = arr[i].length();
+            string temp = "";
+            for(int j=0; j < size; j++){
+                temp += arr[i][j];
+                if((T.noOfPrefixes(temp) <= 1) && (T.string_Ending(temp) == 0) && (j < (size - 1))){
+                    ans.push_back(temp); break;
+                }
+                else if(j == (size - 1)){
+                    ans.push_back(temp); break;
+                }
+            }
+        }
 
-    Trie t = Trie();
-    string T = "Push";
-    t.add_String(T);
-    bool ans = t.Search_String(T);
-    if(ans == true){cout << "Yes" << endl;}
-    else{cout << "No" << endl;}
-}
+        return ans;
+    }
+};
