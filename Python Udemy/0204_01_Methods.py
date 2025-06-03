7d4b6b6336a5aba86efae46b68847c1884e3d9c4
Manas <gupta.manas6268@gmail.com>
2025-06-03 13:12:13 +0530

    Updated

---
+++ Python Udemy/0204_01_Methods.py
@@ -0,0 +1,11 @@
+# Go to Python-Console.
+#  & Type 'dir(str)' or 'dir("anything").
+#        (string)↑             ↑(Any string.)
+#   It gives us all Methods of String in Python.
+
+# Then, 'help(str.capitalize)' or 'help("manas".capitalize)'
+#         ↑(help => Python console helps us with that function.)
+
+# Type, 'import builtins'
+# Then, 'dir(builtins)'
+#           ↑ (It gives us all builtin functions.)
