64bd0df4a0f4fe3119aebe1d941f7e829ea4e0ec
Manas <gupta.manas6268@gmail.com>
2025-06-09 20:30:20 +0530

    Updated

---
+++ Python Udemy/Module-1/2010_ending.py
@@ -0,0 +1,61 @@
+def get_todos(filepath):
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos(filepath, todos_local):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_local)
+
+
+while True:
+    user_action = input("Type add, show, edit, delete or exit: ")
+    user_action = user_action.strip()
+
+    if user_action.startswith("add"):
+        todo = user_action[4:]
+        todos = get_todos("todos.txt")
+        todos.append(todo + '\n')
+        write_todos("todos.txt", todos)
+
+    elif user_action.startswith('show'):
+        todos = get_todos("todos.txt")
+
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{index + 1}-{item}"
+            print(row)
+    
+    elif user_action.startswith('edit'):
+        try:
+            number = int(user_action[5:7])
+
+            number = number - 1
+            todos = get_todos("todos.txt")
+            todos[number] = user_action[8:] + '\n'
+
+            write_todos("todos.txt", todos)
+        except ValueError:
+            print("Your command is not valid.")
+            continue
+
+    elif user_action.startswith('delete'):
+        try:
+            number = int(user_action[9:])
+
+            todos = get_todos("todos.txt")
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
+            write_todos("todos.txt", todos)
+        except:
+            print("There is no item with that number.")
+            continue
+
+    elif user_action.startswith("exit"):
+        break
+
+    else:
+        print("Your command is not valid.")
+
