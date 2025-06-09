f0bf174c358db0472945975f7bd07b740b284bfc
Manas <gupta.manas6268@gmail.com>
2025-06-09 18:59:12 +0530

    Updated

---
+++ Python Udemy/Module-1/1101_functions.py
@@ -0,0 +1,29 @@
+# See line-6. 
+
+
+message = "hello mine"
+def greet_1():
+    message = "hello" # Also, check O/P by commenting this line.
+    message = message.capitalize()
+    new_message = message.capitalize()
+    print("Shower")
+    return new_message
+
+def greet_2():
+    message_local = "Practice coding." 
+    # (↑) It's better to have variable names in function, different from
+    #   global variables, because local variable in function's can sometimes
+    #   change same variable name value unexpectedly.
+    print("2nd function")
+    str = message.isupper()
+
+greeting_1 = greet_1()
+print(greeting_1)
+print(len(greeting_1))
+
+greeting_2 = greet_2()
+print(greeting_2) # none => as there is no 'Return' statement.
+print(len(greeting_2)) # error => because 'greeting_2' has 'NoneType'.
+ 
+# print(new_message) # error => function's variable didn't work outside function.
+print(message)
--- Python Udemy/Module-1/tempCodeRunnerFile.py
+++ Python Udemy/Module-1/tempCodeRunnerFile.py
@@ -1,8 +1,4 @@
-try:
-    waiting_list = ["john", "marry"]
-    name = input("Enter name: ")
-     
-    number = waiting_list.index(name)
-    print(f"{name}'s turn is {number}")
-except ValueError:
-    print(f"{name} is not in list")
+
+str = "report.txt"
+str.strip(6)
+print(str)
