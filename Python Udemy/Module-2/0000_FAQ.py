371152627cffadd1abdd05b46fa0e02c9e704615
Manas <gupta.manas6268@gmail.com>
2025-06-16 00:49:51 +0530

    Updated

---
+++ Python Udemy/Module-2/0000_FAQ.py
@@ -0,0 +1,16 @@
+# Solve 'daily python projects' from https://dailypythonprojects.substack.com/
+#  daily for practice.
+
+
+# See Lecture-189 => FAQ, Question.5:
+
+# See Lecture-203 from [10:00].
+
+
+# In Lecture-203, you already made account in 'figma' 
+#  from 'gupta.manas6268@gmail.com' &
+#  '23cd10ma...@mitsgwl.ac.in' to access for Student-course
+#   whose log-in still isn't completed yet.
+# 
+# I also made account in 'Penpot' => alternative open-souce of 'figma'.
+#  So, learn how to use 'Penpot' also.
---
+++ Python Udemy/Module-3/2701_URL.py
@@ -0,0 +1,9 @@
+import requests
+
+# url = "https://finance.yahoo.com/" 
+# As, above(↑) URL is giving me O/P => "Edge: Too Many Requests"
+url = "https://www.google.com/search?q=yahoo+finance"
+
+request = requests.get(url)
+content = request.text
+print(content)
