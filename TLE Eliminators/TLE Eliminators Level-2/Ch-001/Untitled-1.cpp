da766ce31f4a4dc3a223693c57a9908896a921e8
Manas <gupta.manas6268@gmail.com>
2025-07-21 20:07:32 +0530

    Updated

---
+++ TLE Eliminators/TLE Eliminators Level-2/Ch-001/Untitled-1.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    
+    return 0;
+}
+
+
+
+
+class Solution {
+public:
+    vector<vector<string>> ans;
+    
+    void is_Palindrome()
+    void backtrack(string s, int index){
+
+    }
+    vector<vector<string>> partition(string s) {
+        
+    }
+};
