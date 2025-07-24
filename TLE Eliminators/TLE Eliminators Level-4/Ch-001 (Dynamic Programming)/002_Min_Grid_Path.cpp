0a8fd147f758311c3dc39d5670d1417b21276ed0
Manas <gupta.manas6268@gmail.com>
2025-07-24 21:48:49 +0530

    Updated

---
+++ TLE Eliminators/TLE Eliminators Level-4/Ch-001 (Dynamic Programming)/002_Min_Grid_Path.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long 
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+// Solution
+vector<vector<int>> dp(1e5+10, vector<int> (1e5+10, -1));
+int min_Path(int i, int j, vector<vector<int>>& grid, int n, int m){
+    if((i >= n) || (j >= m)){ // moving outside grid
+        return INF;
+    }
+    if((i == n-1) && (j == m-1)){ // reached destination
+        return grid[n-1][m-1];
+    }
+    if(dp[i][j] != -1){
+        return dp[i][j];
+    }
+
+    dp[i][j] = grid[i][j] + min(min_Path(i, j + 1, grid, n, m), min_Path(i + 1, j, grid, n, m));
+    return dp[i][j];
+}
+
+signed main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    // I/P
+    int n, m; cin >> n >> m;
+    // (1 <= n, m <= 1e5)
+    vector<vector<int>> grid(n);
+    for(int i=0; i < n; i++){
+        vector<int> temp(m);
+        for(int j=0; j < m; j++){ cin >> temp[j];}
+        grid[i] = temp;
+    }
+
+    // O/P
+    int ans = min_Path(0, 0, grid, n, m);
+    cout << ans << endl;
+}
