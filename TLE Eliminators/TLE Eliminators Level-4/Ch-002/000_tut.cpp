71bcf20930b45120a9b87c6e7fc1d281e34bc53b
Manas <gupta.manas6268@gmail.com>
2025-07-30 00:43:11 +0530

    Updated

--- TLE Eliminators/TLE Eliminators Level-4/Ch-002/000_tut.cpp
+++ TLE Eliminators/TLE Eliminators Level-4/Ch-002/000_tut.cpp
@@ -5,3 +5,10 @@
 
 // In Page-4
 //  (B => Budget.)
+
+// In Page-14
+//  (Columns => O(n).)
+
+// In Page-16
+//  = No. of states * Avg T.T    (T.T = Transition Time Per State.)
+//  = N * O(N/2) = O(power(N, 2))
--- TLE Eliminators/TLE Eliminators Level-4/Ch-002/002_Quiz.cpp
+++ TLE Eliminators/TLE Eliminators Level-4/Ch-002/002_Quiz.cpp
@@ -1,2 +1,12 @@
-// From Lecture-1, [01:25:41].
-// 
+// In Slide-1, Page-13
+
+// Quiz-1: Fibonacci Problem?
+// Ans: SC = O(n) => (Wrong)(MLE),
+//      because dp[i] depends on dp[i-1], dp[i-2]
+//      So, SC = O(1) => (Correct.)
+
+// Quiz-2: Grid Problem?
+// Ans:  SC = O(n * m) => (Wrong)(MLE),
+//      because dp[i][j] depends on dp[i+1][j], dp[i][j+1],
+//      i.e. current row only depends upon previous(bottom) row,
+//      So, SC = O(m) => (Correct)
