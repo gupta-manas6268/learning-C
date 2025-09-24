febc64c6bc5b6b63bceb2f70fdb5c1d8bdb85a7c
Manas <gupta.manas6268@gmail.com>
2025-09-24 12:33:22 +0530

    Updated

--- Python Udemy/Module-3/2701_URL.py
+++ Python Udemy/Module-3/2701_URL.py
@@ -1,8 +1,8 @@
 import requests
 
-url = "https://finance.yahoo.com/" 
+# url = "https://finance.yahoo.com/" 
 # As, above(↑) URL is giving me O/P => "Edge: Too Many Requests"
-# url = "https://www.google.com/search?q=yahoo+finance"
+url = "https://www.google.com/search?q=yahoo+finance"
 
 request = requests.get(url)
 content = request.text
