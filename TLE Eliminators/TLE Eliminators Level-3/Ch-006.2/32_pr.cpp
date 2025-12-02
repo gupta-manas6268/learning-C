cdd155b90915dbdf31df730c4e99ba3047c73952
Manas <gupta.manas6268@gmail.com>
2025-12-02 21:32:33 +0530

    Updated

---
+++ TLE Eliminators/TLE Eliminators Level-3/Ch-006.2/32_pr.cpp
@@ -0,0 +1,84 @@
+// Correct.
+
+// B. Alice and the List of Presents
+// https://codeforces.com/problemset/problem/1236/B
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long 
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+// Power
+int power(int base, int exp){
+    int result = 1;
+
+    while(exp > 0){
+        if((exp % 2) == 1){
+            (result *= base) %= MOD;
+
+            (base *= base) %= MOD;
+            exp /= 2;
+        }
+        else{
+            (base *= base) %= MOD;
+            exp /= 2;
+        }
+    }
+
+    return result;
+}
+
+signed main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    // I/P
+    int n, m; cin >> n >> m;
+
+    // O/P
+    int ans = power(power(2,m)-1, n);
+    cout << ans << endl;
+}
